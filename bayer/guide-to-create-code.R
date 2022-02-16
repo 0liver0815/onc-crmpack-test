3fe284be4799105ebddbd829948e11d5da6a14b3
Oliver Boix <oliver.boix@bayer.com>
2022-02-16 12:47:25 +0000

    Add things learned from last merge request

--- bayer/guide-to-create-code.R
+++ bayer/guide-to-create-code.R
@@ -1,3 +1,5 @@
+# general: install packages lintr and styler to style and lint code
+#  this avoid failed checks at the github repository
 # Create the class
 # Add checks that validates the slots of the class in the validity file (e.g. Rules-validity.R)
 # Create function for the user that construct the class
@@ -7,3 +9,4 @@
 # Create unit tests for everything, i.e. for class, method and validity
 # Add new functions to the yaml file
 # add new used R function (e.g. mad) to the list of function in crmPack
+# add new classes to the documentation of the virtual class
